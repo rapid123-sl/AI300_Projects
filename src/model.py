a7f76b06ffc26fd0af9b4a646057a09e0ebb41a8
Samuel Lee Wei Sheng <samuelleeweisheng@gmail.com>
2024-09-23 21:27:53 +0800

    Adding model.py file

---
+++ src/model.py
@@ -0,0 +1,8 @@
+import joblib
+
+class Model:
+    def __init__(self):
+        self.model = joblib.load('model/capstone_catboost_model.pkl')
+    
+    def predict(self, input_features):
+        return self.model.predict(input_features)
